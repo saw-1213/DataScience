8e8ba4341dd7f84036f47b6d3e0a491d86bbcd2b
LAPTOP-L2V3J95I\ACER <yanxu2122@gmail.com>
2025-09-03 22:00:04 +0800

    testing

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load trained model
+model = joblib.load("ann_model.pkl")
+
+st.set_page_config(page_title="Heart Disease Prediction", page_icon="❤️", layout="centered")
+
+st.title("❤️ Heart Disease Prediction App")
+st.write("Enter your health and lifestyle details to predict the risk of Heart Disease.")
