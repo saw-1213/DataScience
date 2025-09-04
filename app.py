75fcc5caf0f06c00c8082e9abb9470d0280a5db0
LAPTOP-L2V3J95I\ACER <yanxu2122@gmail.com>
2025-09-04 12:08:03 +0800

    test lagi 4

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import joblib
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler, MinMaxScaler
 
 # Load trained model
 model = joblib.load("model.pkl")
